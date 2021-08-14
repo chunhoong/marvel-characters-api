9fb59020b75281f9ce1a98eb589f53f4abf31b11
Lim Chun Hoong <chunhoong.lim@outlook.my>
2021-08-14 14:43:00 +0800

    Setup openapi

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import logger from "./core/logger";
 import characterRouter from "./character/characterRouter";
 import {config} from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
 
 config();
 
@@ -9,7 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(undefined, {
+    swaggerOptions: {
+        url: "/docs/openapi.json"
+    }
+}))
 app.use("/characters", characterRouter);
 
 const PORT = process.env.port ? Number(process.env.port) : 8080;
