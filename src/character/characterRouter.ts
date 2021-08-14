cc0268437877e05e2ac9c3283e69cff126361be8
Lim Chun Hoong <chunhoong.lim@outlook.my>
2021-08-14 17:47:41 +0800

    Type strictening

--- src/character/characterRouter.ts
+++ src/character/characterRouter.ts
@@ -1,13 +1,14 @@
-import {Router} from "express";
+import {Request, Response, Router} from "express";
 import characterService from "./characterService";
+import Character from "./character";
 
 const characterRouter = Router();
 
-characterRouter.get("/", async (req, res) => {
+characterRouter.get("/", async (req, res: Response<number[]>) => {
     res.send(await characterService.listCharacterIds());
 });
 
-characterRouter.get("/:id", async (req, res) => {
+characterRouter.get("/:id", async (req: Request<{ id: string }>, res: Response<Character>) => {
     res.send(await characterService.getCharacter(req.params.id));
 });
 
--- src/character/characterService.ts
+++ src/character/characterService.ts
@@ -1,15 +1,16 @@
 import MarvelApi from "../integration/marvelApi";
+import Character from "./character";
 
 class CharacterService {
 
-    async listCharacterIds() {
+    async listCharacterIds(): Promise<number[]> {
         const response = await MarvelApi.listCharacters();
-        return response.data.results.map((result: { id: any; }) => result.id);
+        return response.data.results.map((result: Record<string, unknown>) => result.id);
     }
 
-    async getCharacter(id: string) {
+    async getCharacter(id: string): Promise<Character> {
         const response = await MarvelApi.getCharacter(id);
-        return response.data.results.map((result: { id: any; name: any; description: any; }) => ({
+        return response.data.results.map((result: Record<string, unknown>) => ({
             id: result.id,
             name: result.name,
             description: result.description
