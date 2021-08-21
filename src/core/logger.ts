4431867e49c1891ce9c9bf15a2c3cb7c2efd3345
Lim Chun Hoong <chunhoong.lim@outlook.my>
2021-08-21 09:51:19 +0800

    Change logging framework to log4js

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,5 +1,14 @@
-import {pino} from "pino";
+import log4js from "log4js";
 
-const logger = pino();
+log4js.configure({
+    appenders: {
+        console: { type: "console"}
+    },
+    categories: {
+        default: { appenders: ["console"], level: "info"}
+    }
+});
+
+const logger = log4js.getLogger();
 
 export default logger;
